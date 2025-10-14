1219c10394dbacbc85935b454e5da313a3682836
Robin Brisa <robin.brisa@eliberty.fr>
2025-10-14 17:18:58 +0200

    chore: remove stats functionality

--- Console/Application.php
+++ Console/Application.php
@@ -2,8 +2,6 @@
 
 namespace JMS\JobQueueBundle\Console;
 
-declare(ticks = 10000000);
-
 use Doctrine\DBAL\Statement;
 use Doctrine\DBAL\Types\Type;
 
@@ -29,12 +27,6 @@ class Application extends BaseApplication
         parent::__construct($kernel);
 
         $this->getDefinition()->addOption(new InputOption('--jms-job-id', null, InputOption::VALUE_REQUIRED, 'The ID of the Job.'));
-
-        $kernel->boot();
-        if ($kernel->getContainer()->getParameter('jms_job_queue.statistics')) {
-            $this->insertStatStmt = "INSERT INTO jms_job_statistics (job_id, characteristic, createdAt, charValue) VALUES (:jobId, :name, :createdAt, :value)";
-            register_tick_function(array($this, 'onTick'));
-        }
     }
 
     public function doRun(InputInterface $input, OutputInterface $output)
@@ -50,30 +42,6 @@ class Application extends BaseApplication
             $this->saveDebugInformation($ex);
 
             throw $ex;
-        }
-    }
-
-    public function onTick()
-    {
-        if ( ! $this->input->hasOption('jms-job-id') || null === $jobId = $this->input->getOption('jms-job-id')) {
-            return;
-        }
-
-        $characteristics = array(
-            'memory' => memory_get_usage(),
-        );
-
-        if(!$this->insertStatStmt instanceof Statement){
-            $this->insertStatStmt = $this->getConnection()->prepare($this->insertStatStmt);
-        }
-
-        $this->insertStatStmt->bindValue('jobId', $jobId, \PDO::PARAM_INT);
-        $this->insertStatStmt->bindValue('createdAt', new \DateTime(), Type::getType('datetime'));
-
-        foreach ($characteristics as $name => $value) {
-            $this->insertStatStmt->bindValue('name', $name);
-            $this->insertStatStmt->bindValue('value', $value);
-            $this->insertStatStmt->execute();
         }
     }
 
